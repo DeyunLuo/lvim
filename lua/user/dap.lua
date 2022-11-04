8ccbfaa476332cb9d7b9e102270f93f037453516
deyun.luo <deyun.luo@baishan.com>
2022-11-04 11:39:30 +0800

    fix: init

---
+++ lua/user/dap.lua
@@ -0,0 +1,108 @@
+local M = {}
+
+M.config = function()
+  local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+  local function sep_os_replacer(str)
+    local result = str
+    local path_sep = package.config:sub(1, 1)
+    result = result:gsub("/", path_sep)
+    return result
+  end
+
+  local join_path = require("lvim.utils").join_paths
+
+  local status_ok, dap = pcall(require, "dap")
+  if not status_ok then
+    return
+  end
+
+  dap.adapters.go = function(callback, _)
+    local stdout = vim.loop.new_pipe(false)
+    local handle
+    local pid_or_err
+    local port = 38697
+    local opts = {
+      stdio = { nil, stdout },
+      args = { "dap", "-l", "127.0.0.1:" .. port },
+      detached = true,
+    }
+    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+      stdout:close()
+      handle:close()
+      if code ~= 0 then
+        print("dlv exited with code", code)
+      end
+    end)
+    assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require("dap.repl").append(chunk)
+        end)
+      end
+    end)
+    -- Wait for delve to start
+    vim.defer_fn(function()
+      callback { type = "server", host = "127.0.0.1", port = port }
+    end, 100)
+  end
+  -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+  dap.configurations.go = {
+    {
+      type = "go",
+      name = "Debug",
+      request = "launch",
+      program = "${file}",
+    },
+    {
+      type = "go",
+      name = "Debug test", -- configuration for debugging test files
+      request = "launch",
+      mode = "test",
+      program = "${file}",
+    },
+    -- works with go.mod packages and sub packages
+    {
+      type = "go",
+      name = "Debug test (go.mod)",
+      request = "launch",
+      mode = "test",
+      program = "./${relativeFileDirname}",
+    },
+  }
+
+  local path = vim.fn.glob(mason_path .. "packages/codelldb/extension/")
+      or vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.8.1/"
+  local lldb_cmd = path .. "adapter/codelldb"
+
+  dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      -- CHANGE THIS to your path!
+      command = lldb_cmd,
+      args = { "--port", "${port}" },
+
+      -- On windows you may have to uncomment this:
+      -- detached = false,
+    },
+  }
+
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = true,
+    },
+  }
+  dap.configurations.c = dap.configurations.cpp
+
+end
+
+return M
